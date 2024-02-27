d9ac141ebfe0a2cdf4579fd8ab7758ca27e4f2a9
safi <safiou102003@gmail.com>
2024-02-26 20:24:52 -0800

    fix : navBar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -10,20 +10,22 @@ import {
 
 export default function Nav() {
   return (
-    <header className="flex mx-3.5 w-full items-center justify-between p-2.5 ">
+    <header className="flex mx-3.5 w-full items-center justify-between ">
       <Image src={logo} width={100} height={100} alt="Picture of the author" />
 
-      <div className="flex gap-3">
-        <input
-          type="text"
-          className="py-3 px-4 md:w-96 border-2 border-gray-200 border-solid rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-          placeholder="Search your product here"
-        />
-        <FontAwesomeIcon
-          icon={faMagnifyingGlass}
-          className="self-center mt-1 md:mt-0"
-        />
-      </div>
+      <div className="relative">
+  <FontAwesomeIcon
+    icon={faMagnifyingGlass}
+    className="absolute inset-y-0 left-0 pl-3 self-center mt-3 md:mt-0 text-gray-400 pointer-events-none"
+  />
+  <input
+    type="text"
+    className="py-3 px-4 pl-10 md:w-96 h-9 border-2 border-gray-200 border-solid rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+    placeholder="Search your product here"
+  />
+</div>
+
+
 
       <div className="flex justify-start items-center gap-[15px] p-2.5 m-1.5  ">
         <FontAwesomeIcon icon={faCartShopping} className="text-xl font-light" />
