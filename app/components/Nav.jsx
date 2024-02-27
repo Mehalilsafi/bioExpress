f2f3d0214ffd364d38036cc729bf5d2ffa92f8bd
safi <safiou102003@gmail.com>
2024-02-26 20:56:55 -0800

    fix (sideBare.jsx):  add style

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -10,27 +10,27 @@ import {
 
 export default function Nav() {
   return (
-    <header className="flex mx-3.5 w-full items-center justify-between ">
+    <header className="flex mx-3 w-full items-center justify-between ">
       <Image src={logo} width={100} height={100} alt="Picture of the author" />
 
       <div className="relative">
-  <FontAwesomeIcon
-    icon={faMagnifyingGlass}
-    className="absolute inset-y-0 left-0 pl-3 self-center mt-3 md:mt-0 text-gray-400 pointer-events-none"
-  />
-  <input
-    type="text"
-    className="py-3 px-4 pl-10 md:w-96 h-9 border-2 border-gray-200 border-solid rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-    placeholder="Search your product here"
-  />
-</div>
-
-
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className="absolute inset-y-0 left-0 pl-3 self-center mt-3  text-gray-400 pointer-events-none md:mt-3 "
+        />
+        <input
+          type="text"
+          className="py-3 px-4 pl-10 md:w-96 h-9 border-2 border-gray-200 border-solid rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+          placeholder="Search your product here"
+        />
+      </div>
 
       <div className="flex justify-start items-center gap-[15px] p-2.5 m-1.5  ">
         <FontAwesomeIcon icon={faCartShopping} className="text-xl font-light" />
         <FontAwesomeIcon icon={faUser} className="text-xl font-light" />
       </div>
+
+
     </header>
   );
 }
--- app/components/SideBar.jsx
+++ app/components/SideBar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import sidBarImage from "@/assets/sideBarImage.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPizzaSlice,
@@ -7,6 +9,7 @@ import {
   faWandMagicSparkles,
   faGlobe,
   faComment,
+  faTable,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function SideBar() {
@@ -21,7 +24,7 @@ export default function SideBar() {
     { icon: faComment, text: "About Us" },
   ];
   return (
-    <div>
+    <div className=" m-3">
       <button
         type="button"
         className="text-gray-500 hover:text-gray-600"
@@ -31,7 +34,7 @@ export default function SideBar() {
       >
         <span className="sr-only">Toggle Navigation</span>
         <svg
-          className="flex-shrink-0 size-4"
+          className="flex-shrink-0 size-6 md:hidden"
           width="16"
           height="16"
           fill="currentColor"
@@ -48,15 +51,22 @@ export default function SideBar() {
         id="docs-sidebar"
         className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700"
       >
-        <div className="px-6">
+        <div className="px-6 flex align-middle gap-3">
+          <FontAwesomeIcon
+            icon={faTable}
+            className="text-xl font-light text-[#A4CE4A]"
+          />
           <h2 className="flex-none text-xl font-semibold dark:text-white">
             {" "}
             categories
           </h2>
         </div>
-        <div className="flex flex-col w-full justify-center gap-10">
+        <div className="flex flex-col w-full justify-center gap-4 m-3 border-b-2 border-gray-200 border-solid">
           {items.map((item, index) => (
-            <div key={index} className="flex items-center">
+            <div
+              key={index}
+              className="flex justify-start items-center p-2 gap-3"
+            >
               <FontAwesomeIcon
                 icon={item.icon}
                 className="text-xl font-light"
@@ -66,17 +76,27 @@ export default function SideBar() {
           ))}
         </div>
 
-        <div className="flex flex-col w-full justify-center gap-10">
+        <div className="flex flex-col w-full justify-center gap-4 m-3">
           {itemsButtom.map((item, index) => (
-            <div key={index} className="flex items-center">
+            <div
+              key={index}
+              className="flex items-center justify-start  p-2 gap-3"
+            >
               <FontAwesomeIcon
                 icon={item.icon}
-                className="text-xl font-light"
+                className="text-xl font-light text-[#A4CE4A]"
               />
               <p>{item.text}</p>
             </div>
           ))}
         </div>
+        <Image
+          src={sidBarImage}
+          width={300}
+          height={300}
+          alt="Picture of the author"
+          className="m-3 rounded-md pr-6"
+        />
       </div>
     </div>
   );
