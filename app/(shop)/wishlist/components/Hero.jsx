07dbb2a1d6ed6ee5f392e4dd038a4152703c1857
safi <safiou102003@gmail.com>
2024-03-15 07:55:57 -0700

    feat : add  Prduct components

--- app/(shop)/wishlist/components/Hero.jsx
+++ app/(shop)/wishlist/components/Hero.jsx
@@ -4,12 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faTrash } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import olive from "@/assets/olive.svg";
-import Counter from "./Counter";
+import Product from "./Product";
 import { useCartStore } from "@/lib/stor";
 import getProduct from "@/lib/getProduct";
 export default function Hero() {
   const cartItems = useCartStore((state) => state.cartItems);
   const Products = getProduct();
+
   console.log("prduct Data is here " + cartItems);
 
   return (
@@ -46,37 +47,8 @@ export default function Hero() {
         </button>
       </div>
       <div className=" flex flex-col md:flex-row gap-5 mt-5">
-        <div className="flex flex-col gap-5 ">
-          {Products.map((product) => (
-            <div
-              key={product}
-              className="flex items-center h-[200px] p-3 gap-4 rounded bg-white"
-            >
-              <input
-                type="checkbox"
-                className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                id={`hs-default-checkbox-${product}`}
-              />
-              <Image
-                src={product.url}
-                width={100}
-                height={100}
-                alt="Picture of the author"
-              />
-              <div className="flex flex-col justify-center items-start">
-                <h1 className="font-bold text-2xl">{product.productName}</h1>
-                <p className="max-w-lg text-center">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Necessitatibus officiis adipisci nostrum
-                </p>
-              </div>
-              <div className="flex items-end gap-[10px]">
-                <Counter />
-                <FontAwesomeIcon icon={faTrash} className="mb-3" />
-              </div>
-            </div>
-          ))}
-        </div>
+        <Product Products={Products} />
+
         <div>
           <Image
             src={olive}
---
+++ app/(shop)/wishlist/components/Product.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Image from 'next/image'
+import { faCartShopping, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Counter from "./Counter";
+export default function Product({Products}) {
+  return (
+    
+    <div className="flex flex-col gap-5 ">
+    {Products.map((product) => (
+      <div
+        key={product}
+        className="flex items-center h-[200px] p-3 gap-4 rounded bg-white"
+      >
+        <input
+          type="checkbox"
+          className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+          id={`hs-default-checkbox-${product}`}
+        />
+        <Image
+          src={product.url}
+          width={100}
+          height={100}
+          alt="Picture of the author"
+        />
+        <div className="flex flex-col justify-center items-start">
+          <h1 className="font-bold text-2xl">{product.productName}</h1>
+          <p className="max-w-lg text-center">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Necessitatibus officiis adipisci nostrum
+          </p>
+        </div>
+        <div className="flex items-end gap-[10px]">
+          <Counter />
+          <FontAwesomeIcon icon={faTrash} className="mb-3" />
+        </div>
+      </div>
+    ))}
+  </div>
+  )
+}
