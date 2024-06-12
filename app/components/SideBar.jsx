827eb4ebd63a503cf0d48901998b5c9a4be58edf
safi <safiou102003@gmail.com>
2024-06-12 09:00:40 -0700

    feat:logo to sibd bare

--- app/components/SideBar.jsx
+++ app/components/SideBar.jsx
@@ -15,6 +15,7 @@ import {
   faJar,
 } from "@fortawesome/free-solid-svg-icons";
 import { useCategory } from "@/lib/stor";
+import logo from "@/assets/logo.svg"; // Image asset for logo
 
 export default function SideBar({ publicity, categories }) {
   const items = categories?.map((category, index) => ({
@@ -65,6 +66,7 @@ export default function SideBar({ publicity, categories }) {
         id="docs-sidebar"
         className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700 md:mt-[100px]  border border-transparent"
       >
+        <Image src={logo} width={130} className="md:hidden" alt="Picture of the author"  />
         <div className="px-6 flex align-middle gap-3">
           <FontAwesomeIcon
             icon={faTable}
