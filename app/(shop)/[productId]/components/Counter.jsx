f9592f7141e6f65850507baa5cb688e23c6a8c5a
safi <safiou102003@gmail.com>
2024-06-16 10:40:30 -0700

    fix :quantity limit

--- app/(shop)/[productId]/components/Counter.jsx
+++ app/(shop)/[productId]/components/Counter.jsx
@@ -2,10 +2,19 @@
 
 import React, { useState } from "react";
 import { useStore } from "@/lib/stor";
-export default function Counter() {
+import { toast } from "react-toastify";
+export default function Counter({maxQuantity }) {
   const counter = useStore((state) => state.counter);
+
   const increaseCounter = useStore((state) => state.increaseCounter);
   const decreaseCounter = useStore((state) => state.decreaseCounter);
+  function handleIncrement() {
+    if (counter < maxQuantity ) {
+      increaseCounter();
+    } else {
+      toast.error('You have reached the maximum allowable quantity for this product.');
+    }
+  }
 
   return (
     <div className="flex gap-2  items-center">
@@ -17,7 +26,7 @@ export default function Counter() {
       </button>
       <p className="text-xl font-bold">{counter}</p>
       <button
-        onClick={increaseCounter}
+        onClick={handleIncrement}
         className="px-4 py-2 bg-[#C5DCA0] text-white font-semibold rounded-lg hover:bg-opacity-80"
       >
         +
--- app/(shop)/[productId]/components/Product.jsx
+++ app/(shop)/[productId]/components/Product.jsx
@@ -11,26 +11,24 @@ import CarouselThumbs from "./CarouselThumbs";
 import { useStore } from "@/lib/stor";
 import { useRouter } from "next/navigation";
 import getProductId from "../../actions/getProductId";
+import { toast } from "react-toastify";
 
-export default function Product({product}) {
- 
-  const router=useRouter()
-  function handlRout(){
-     router.push(`/${product.id}/${product.productName}`)
+export default function Product({ product }) {
+  const router = useRouter();
+  function handlRout() {
+    router.push(`/${product.id}/${product.productName}`);
   }
   const counter = useStore((state) => state.counter);
   const totalPrice = counter * product.productPrice;
+  const maxQuantity = Math.floor(product?.quantity * 0.7);
 
   return (
     <div className=" flex flex-col gap-3 md:gap-6  mt-4 md:w-6/12">
       <h1 className="font-medium text-3xl"> {product?.productName} </h1>
-      <p className="font-medium text-sm ">
-      {product?.description}
-      </p>
+      <p className="font-medium text-sm ">{product?.description}</p>
       <h1 className="text-xl font-semibold  text-[#996534]">{totalPrice}</h1>
-      <Counter />
+      <Counter maxQuantity={maxQuantity} />
       <div className="flex gap-[10px] ">
-     
         <button
           type="button"
           className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-[#A4CE4A] hover:border-[#eaffe9]  disabled:opacity-50 disabled:pointer-events-none dark:border-white dark:text-white dark:hover:text-gray-300 dark:hover:border-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 hover:bg-[#A4CE4A] "
