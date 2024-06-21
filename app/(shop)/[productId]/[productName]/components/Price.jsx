3aa1bb6110cbd9c6b0d2d6fb142407e3a52bbbad
safi <safiou102003@gmail.com>
2024-06-21 11:55:04 -0700

    fix:update price based on quantity

--- app/(shop)/[productId]/[productName]/components/Price.jsx
+++ app/(shop)/[productId]/[productName]/components/Price.jsx
@@ -1,34 +1,45 @@
+"use client"
 import React from "react";
 import Image from "next/image";
+import { useStore } from "@/lib/stor";
 export default function Price({ product }) {
   const productImages = product?.images;
 
   const firstImageUrl =
     productImages && productImages?.length > 0 ? productImages[0] : null;
-  return (
-    <div className="lg:border-l-[1px] lg:border-inherit lg:border-solid h-full ">
-      <div className="flex flex-row justify-between m-4 w-full ">
-        <div className="flex  flex-row  gap-3">
-          <Image
-            width={100}
-            height={100}
-            src={firstImageUrl}
-            alt="product image"
-          />
-          <p>{product?.productName}</p>
+    const UserQuantity = useStore((state) => state.counter);
+    const quantity = UserQuantity;
+    const productPrice = product?.productPrice || 0;
+    const subTotal = productPrice * quantity;
+    const total = subTotal; // Here you can add more calculations if needed (e.g., tax, shipping)
+  
+    return (
+      <div className="lg:border-l-[1px] lg:border-inherit lg:border-solid h-full">
+        <div className="flex flex-row justify-between m-4 w-full">
+          <div className="flex flex-row gap-3">
+            <Image
+              width={100}
+              height={100}
+              src={firstImageUrl}
+              alt="product image"
+            />
+            <div className="flex flex-col justify-center">
+              <p>{product?.productName}</p>
+              <p>Quantity: {quantity}</p>
+            </div>
+          </div>
+          <p>{`${productPrice} DA`}</p>
         </div>
-        <p> {`${product?.productPrice} DA`}</p>
-      </div>
-      <div className="flex flex-col  m-4 gap-4">
-        <div className="flex flex-row justify-between">
-          <p>sub-total</p>
-          <p>{`${product?.productPrice} DA`}</p>
+        <div className="flex flex-col m-4 gap-4">
+          <div className="flex flex-row justify-between">
+            <p>Sub-total</p>
+            <p>{`${subTotal} DA`}</p>
+          </div>
+        </div>
+        <div className="flex flex-row justify-between m-4 w-full">
+          <p>Total</p>
+          <p>{`${total} DA`}</p>
         </div>
       </div>
-      <div className="flex flex-row justify-between m-4 w-full">
-        <p>Total</p>
-        <p>{`${product?.productPrice} DA`}</p>
-      </div>
-    </div>
-  );
-}
+    );
+  };
